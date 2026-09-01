2815f6a6f83dbf9afe4f1c47f95ac930445f8617
Bheesham Persaud <bhee@mozilla.com>
2026-09-01 15:31:32 -0400

    Temporarily support both LDAP and PMO groups
    This commit should be safely reversible. The goal: once all users are in
    the correct groups and have refreshed their sessions, we can revert this
    commit to finalize the groups being in PMO.
    
    Jira: IAM-2043

--- tf/actions/samlMappings.js
+++ tf/actions/samlMappings.js
@@ -76,26 +76,26 @@ exports.onExecutePostLogin = async (event, api) => {
     case "cEfnJekrSStxxxBascTjNEDAZVUPAIU2": {
       const groupToRoles = [
         {
-          group: "mozilliansorg_stripe_subplat_admin",
+          group: "stripe_subplat_admin",
           users: ["araccounting@mozilla.com"],
           roles: [{ role: "admin", account: "acct_1EJOaaJNcmPzuWtR" }],
         },
         {
-          group: "mozilliansorg_stripe_subplat_developer",
+          group: "stripe_subplat_developer",
           roles: [{ role: "developer", account: "acct_1EJOaaJNcmPzuWtR" }],
         },
         {
-          group: "mozilliansorg_stripe_subplat_supportsp",
+          group: "stripe_subplat_supportsp",
           roles: [
             { role: "support_specialist", account: "acct_1EJOaaJNcmPzuWtR" },
           ],
         },
         {
-          group: "mozilliansorg_stripe_subplat_analyst",
+          group: "stripe_subplat_analyst",
           roles: [{ role: "analyst", account: "acct_1EJOaaJNcmPzuWtR" }],
         },
         {
-          group: "mozilliansorg_stripe_subplat_viewonly",
+          group: "stripe_subplat_viewonly",
           roles: [{ role: "view_only", account: "acct_1EJOaaJNcmPzuWtR" }],
         },
       ];
@@ -103,7 +103,9 @@ exports.onExecutePostLogin = async (event, api) => {
       for (const rule of groupToRoles) {
         const usersAllowed = rule.users ?? [];
         const isAllowedByEmail = usersAllowed.includes(event.user.email);
-        const isAllowedByGroup = userGroups.includes(rule.group);
+        const isAllowedByGroup =
+          userGroups.includes(rule.group) ||
+          userGroups.includes(`mozilliansorg_${rule.group}`);
         if (!(isAllowedByEmail || isAllowedByGroup)) {
           continue;
         }
